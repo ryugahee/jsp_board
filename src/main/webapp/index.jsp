098e918c4423a234abe73033715f941a41c3da79
ryugahee <gagml910@hanmail.net>
2023-08-31 03:09:02 +0900

    init

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP 게시판 웹 사이트</title>
+</head>
+<body>
+    <script>
+        location.href = 'login.jsp';
+    </script>
+</body>
+</html>
---
+++ src/user/User.java
@@ -0,0 +1,49 @@
+package src.user;
+
+public class User {
+    private String userID;
+    private String userPasseord;
+    private String userName;
+    private String userGender;
+    private String userEmail;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserPasseord() {
+        return userPasseord;
+    }
+
+    public void setUserPasseord(String userPasseord) {
+        this.userPasseord = userPasseord;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserGender() {
+        return userGender;
+    }
+
+    public void setUserGender(String userGender) {
+        this.userGender = userGender;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
